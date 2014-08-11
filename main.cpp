8e544fae2c0466b75ea2a65ca4b52199af6375ab
Brecht Verstichel <brecht.verstichel@ugent.be>
2014-08-11 17:37:00 +0200

    run!

--- main.cpp
+++ main.cpp
@@ -29,14 +29,14 @@ int main(int argc,char *argv[]){
    int D = atoi(argv[3]);//virtual dimension
    int D_aux = atoi(argv[4]);//auxiliary dimension for the contraction
 
-   double tau = 0.01;
+   double dtau = 0.01;
 
    //initialize some statics dimensions
    Global::lat.set(L,L,d);
    Environment::init();
    Heisenberg::init();
 
-   Trotter::set(tau);
+   Trotter::set(dtau);
 
    PEPS<double> peps(D);
    peps.initialize_state_sum(D);
@@ -44,7 +44,7 @@ int main(int argc,char *argv[]){
    peps.normalize(D_aux);
 
    Environment::calc_env('A',peps,D_aux);
-/*
+
    for(int i = 0;i < 1000;++i){
 
       propagate::step(peps,D_aux);
@@ -57,5 +57,140 @@ int main(int argc,char *argv[]){
       }
 
    }
-*/
+
+   dtau /= 10.0;
+
+   cout << endl;
+   cout << "Timestep dt = " << dtau << endl;
+
+   for(int i = 1000;i < 2000;++i){
+
+      propagate::step(peps,D_aux);
+
+      if(i % 10 == 0){
+
+         Environment::calc_env('A',peps,D_aux);
+         cout << i << "\t" << Heisenberg::energy(peps) << endl;
+
+      }
+
+   }
+
+   //up to D=3
+   D++;
+   peps.grow_bond_dimension(D,0.0001);
+
+   D_aux = D*D;
+
+   dtau = 0.01;
+
+   for(int i = 2000;i < 3000;++i){
+
+      propagate::step(peps,D_aux);
+
+      if(i % 10 == 0){
+
+         Environment::calc_env('A',peps,D_aux);
+         cout << i << "\t" << Heisenberg::energy(peps) << endl;
+
+      }
+
+   }
+
+   dtau /= 10.0;
+
+   cout << endl;
+   cout << "Timestep dt = " << dtau << endl;
+
+   for(int i = 3000;i < 4000;++i){
+
+      propagate::step(peps,D_aux);
+
+      if(i % 10 == 0){
+
+         Environment::calc_env('A',peps,D_aux);
+         cout << i << "\t" << Heisenberg::energy(peps) << endl;
+
+      }
+
+   }
+
+   //up to D=4
+   D++;
+   peps.grow_bond_dimension(D,0.0001);
+
+   D_aux = D*D;
+
+   dtau = 0.01;
+
+   for(int i = 4000;i < 5000;++i){
+
+      propagate::step(peps,D_aux);
+
+      if(i % 10 == 0){
+
+         Environment::calc_env('A',peps,D_aux);
+         cout << i << "\t" << Heisenberg::energy(peps) << endl;
+
+      }
+
+   }
+
+   dtau /= 10.0;
+
+   cout << endl;
+   cout << "Timestep dt = " << dtau << endl;
+
+   for(int i = 6000;i < 7000;++i){
+
+      propagate::step(peps,D_aux);
+
+      if(i % 10 == 0){
+
+         Environment::calc_env('A',peps,D_aux);
+         cout << i << "\t" << Heisenberg::energy(peps) << endl;
+
+      }
+
+   }
+
+   //finally D=5
+   D++;
+   peps.grow_bond_dimension(D,0.0001);
+
+   D_aux = D*D;
+
+   dtau = 0.01;
+
+   for(int i = 7000;i < 8000;++i){
+
+      propagate::step(peps,D_aux);
+
+      if(i % 10 == 0){
+
+         Environment::calc_env('A',peps,D_aux);
+         cout << i << "\t" << Heisenberg::energy(peps) << endl;
+
+      }
+
+   }
+
+   dtau /= 10.0;
+
+   cout << endl;
+   cout << "Timestep dt = " << dtau << endl;
+
+   for(int i = 8000;i < 9000;++i){
+
+      propagate::step(peps,D_aux);
+
+      if(i % 10 == 0){
+
+         Environment::calc_env('A',peps,D_aux);
+         cout << i << "\t" << Heisenberg::energy(peps) << endl;
+
+      }
+
+   }
+
 }
